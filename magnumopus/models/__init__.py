c3bcdff043b3d8adc69e76e4cc1106288de73e50
Phil Weir <phil.weir@flaxandteal.co.uk>
2023-02-25 18:18:33 +0000

    make db settable

--- magnumopus/models/__init__.py
+++ magnumopus/models/__init__.py
@@ -1,5 +1,6 @@
+import os
 from .base import db
 
 def init_app(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:////tmp/test.db')
     db.init_app(app)
